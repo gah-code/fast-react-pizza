57913e4252c1166a1466b2bf2b0a40bd50464bc1
Gilbert Haro <gilbertharocode@gmail.com>
2024-12-05 15:24:03 -0800

    tailwinds base setup

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,13 +8,13 @@ function AppLayout() {
   const isLoading = navigation.state === 'loading';
 
   return (
-    <div className='layout'>
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
       <Header />
 
-      <div className='overflow-scroll'>
-        <main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
           <Outlet />
         </main>
       </div>
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,18 +1,15 @@
 import { Link } from 'react-router-dom';
-import Username from '../features/user/Username';
 import SearchOrder from '../features/order/SearchOrder';
-// import Username from '../features/user/Username';
+import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header>
-      <Link to='/' className='tracking-widest'>
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
-      <SearchOrder />
 
-      {/* <SearchOrder />
-      <Username /> */}
+      <SearchOrder />
       <Username />
     </header>
   );
